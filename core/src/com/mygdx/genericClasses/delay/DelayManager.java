d3895c33dd443da36c257e3e76015319a59bb717
Gilgamesh64 <chto24io@gmail.com>
2024-09-09 17:55:39 +0200

    tiled movement style collisions

---
+++ core/src/com/mygdx/genericClasses/delay/DelayManager.java
@@ -0,0 +1,28 @@
+package com.mygdx.genericClasses.delay;
+
+import java.util.HashMap;
+
+public class DelayManager {
+    private static HashMap<Object, Integer> originalDelays;
+    private static HashMap<Object, Integer> currentDelays;
+
+    public static void registerObject(Object o, Integer time){
+        originalDelays.put(o, time);
+        currentDelays.put(o, time);
+    }
+    public static void unregisterObject(Object o){
+        originalDelays.remove(o);
+        currentDelays.remove(o);
+    }
+    public static void resetDelay(Object o){
+        currentDelays.replace(o, originalDelays.get(o));
+    }
+    public static void updateDelay(Object o){
+        if(currentDelays.get(o) > 0) {
+            currentDelays.replace(o, currentDelays.get(o) - 1);
+        }
+    }
+    public static Integer getCurrentDelay(Object o){
+        return currentDelays.get(o);
+    }
+}
