8aeb08e561f635e5291cc0f06298304b0e266620
Gilgamesh64 <chto24io@gmail.com>
2024-10-13 08:32:10 +0200

    fixing player sheet

---
+++ assets/map/sandstone/sandstone_tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sandstone_tileset" tilewidth="32" tileheight="32" spacing="8" margin="4" tilecount="6" columns="3">
+ <image source="Sandstone tileset 4px border.png" width="120" height="81"/>
+ <tile id="5">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- core/src/com/mygdx/resources/ResourceManager.java
+++ core/src/com/mygdx/resources/ResourceManager.java
@@ -24,7 +24,7 @@ public class ResourceManager {
     }
 
     private void loadPathMap() {
-        pathMap.put(ResourceEnum.PLAYER, "animation_sheet.png");
+        pathMap.put(ResourceEnum.PLAYER, "playerSpritesheet.png");
         pathMap.put(ResourceEnum.STONE, "bullets/stone.png");
         pathMap.put(ResourceEnum.BULLET, "bullets/bullet.png");
         pathMap.put(ResourceEnum.DEFAULT, "weapons/default.png");
