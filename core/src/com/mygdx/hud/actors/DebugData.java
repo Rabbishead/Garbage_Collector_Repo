f79ad81f6a7c77ad26fd48fdce0ea964880e16d1
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2025-04-10 16:50:50 +0200

    debuh twxrt

--- core/src/com/mygdx/hud/actors/DebugData.java
+++ core/src/com/mygdx/hud/actors/DebugData.java
@@ -2,12 +2,14 @@ package com.mygdx.hud.actors;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Disposable;
 import com.mygdx.Data;
 
 public class DebugData extends Actor implements Disposable{
     private final BitmapFont font;
+    GlyphLayout g = new GlyphLayout();
 
     private String text = "";
 
@@ -17,7 +19,9 @@ public class DebugData extends Actor implements Disposable{
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        font.draw(batch, "Debug: =" + text, Data.VIEWPORT_X-128,
+        g.setText(font, "Debug: =" + text);
+        System.out.println(g.width);
+        font.draw(batch, g, Data.VIEWPORT_X-128,
                 getStage().getCamera().viewportHeight);
     }
 
