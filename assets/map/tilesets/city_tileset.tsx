be3731136282c4d8e68ae2a6f6042860cbc7c75c
Gilgamesh64 <chto24io@gmail.com>
2025-05-15 10:36:59 +0200

    invisible doors

--- assets/map/tilesets/city_tileset.tsx
+++ assets/map/tilesets/city_tileset.tsx
@@ -1,67 +1,144 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="city_tileset" tilewidth="32" tileheight="32" spacing="8" margin="4" tilecount="42" columns="6">
- <image source="../images/citytiles.png" width="240" height="280"/>
- <tile id="15">
+<tileset version="1.10" tiledversion="1.10.2" name="city_tileset" tilewidth="32" tileheight="32" spacing="8" margin="4" tilecount="100" columns="10">
+ <image source="../images/citytiles.png" width="400" height="400"/>
+ <tile id="23">
   <animation>
-   <frame tileid="15" duration="360"/>
-   <frame tileid="21" duration="360"/>
-   <frame tileid="27" duration="360"/>
+   <frame tileid="23" duration="360"/>
    <frame tileid="33" duration="360"/>
-   <frame tileid="39" duration="360"/>
+   <frame tileid="43" duration="360"/>
+   <frame tileid="53" duration="360"/>
+   <frame tileid="63" duration="360"/>
   </animation>
  </tile>
- <tile id="17">
+ <tile id="42">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="70">
   <properties>
    <property name="destination" value="PARK-SLUMS"/>
    <property name="name" value="SLUMS-PARK"/>
    <property name="orientation" value="d"/>
   </properties>
  </tile>
- <tile id="23">
+ <tile id="71">
   <properties>
    <property name="destination" value="SLUMS-PARK"/>
    <property name="name" value="PARK-SLUMS"/>
    <property name="orientation" value="a"/>
   </properties>
  </tile>
- <tile id="26">
-  <properties>
-   <property name="blocked" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="29">
+ <tile id="72">
   <properties>
    <property name="destination" value="RICH_DISTRICT-PARK"/>
    <property name="name" value="PARK-RICH_DISTRICT"/>
    <property name="orientation" value="d"/>
   </properties>
  </tile>
- <tile id="34">
-  <properties>
-   <property name="destination" value="REFLECTION_ARENA-RICH_DISTRICT"/>
-   <property name="name" value="RICH_DISTRICT-REFLECTION_ARENA"/>
-   <property name="orientation" value="s"/>
-  </properties>
- </tile>
- <tile id="35">
+ <tile id="73">
   <properties>
    <property name="destination" value="PARK-RICH_DISTRICT"/>
    <property name="name" value="RICH_DISTRICT-PARK"/>
    <property name="orientation" value="a"/>
   </properties>
  </tile>
- <tile id="40">
+ <tile id="74">
+  <properties>
+   <property name="destination" value="REFLECTION_ARENA-RICH_DISTRICT"/>
+   <property name="name" value="RICH_DISTRICT-REFLECTION_ARENA"/>
+   <property name="orientation" value="s"/>
+  </properties>
+ </tile>
+ <tile id="75">
   <properties>
    <property name="destination" value="RICH_DISTRICT-REFLECTION_ARENA"/>
    <property name="name" value="REFLECTION_ARENA-RICH_DISTRICT"/>
    <property name="orientation" value="w"/>
   </properties>
  </tile>
- <tile id="41">
+ <tile id="80">
   <properties>
-   <property name="destination" value="CITY_ROOM_1_DOOR_3"/>
-   <property name="name" value="CITY_ROOM_3_DOOR_2"/>
+   <property name="destination" value="PARK-SLUMS"/>
+   <property name="name" value="SLUMS-PARK"/>
+   <property name="orientation" value="d"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="destination" value="SLUMS-PARK"/>
+   <property name="name" value="PARK-SLUMS"/>
    <property name="orientation" value="a"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="destination" value="RICH_DISTRICT-PARK"/>
+   <property name="name" value="PARK-RICH_DISTRICT"/>
+   <property name="orientation" value="d"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="destination" value="PARK-RICH_DISTRICT"/>
+   <property name="name" value="RICH_DISTRICT-PARK"/>
+   <property name="orientation" value="a"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="destination" value="REFLECTION_ARENA-RICH_DISTRICT"/>
+   <property name="name" value="RICH_DISTRICT-REFLECTION_ARENA"/>
+   <property name="orientation" value="s"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="destination" value="RICH_DISTRICT-REFLECTION_ARENA"/>
+   <property name="name" value="REFLECTION_ARENA-RICH_DISTRICT"/>
+   <property name="orientation" value="w"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="destination" value="PARK-SLUMS"/>
+   <property name="name" value="SLUMS-PARK"/>
+   <property name="orientation" value="d"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="destination" value="SLUMS-PARK"/>
+   <property name="name" value="PARK-SLUMS"/>
+   <property name="orientation" value="a"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="destination" value="RICH_DISTRICT-PARK"/>
+   <property name="name" value="PARK-RICH_DISTRICT"/>
+   <property name="orientation" value="d"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="destination" value="PARK-RICH_DISTRICT"/>
+   <property name="name" value="RICH_DISTRICT-PARK"/>
+   <property name="orientation" value="a"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="destination" value="REFLECTION_ARENA-RICH_DISTRICT"/>
+   <property name="name" value="RICH_DISTRICT-REFLECTION_ARENA"/>
+   <property name="orientation" value="s"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="destination" value="RICH_DISTRICT-REFLECTION_ARENA"/>
+   <property name="name" value="REFLECTION_ARENA-RICH_DISTRICT"/>
+   <property name="orientation" value="w"/>
   </properties>
  </tile>
 </tileset>
