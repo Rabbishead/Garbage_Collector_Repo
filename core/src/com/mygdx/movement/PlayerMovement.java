8e8aaf41f8eb6bbd40d0bc87a3033005a88ec92c
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2025-10-15 16:22:42 +0200

    player moving faster where he is seeing

--- core/src/com/mygdx/movement/PlayerMovement.java
+++ core/src/com/mygdx/movement/PlayerMovement.java
@@ -3,8 +3,8 @@ package com.mygdx.movement;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-
+import com.mygdx.controllers.camera.CameraController;
+import com.mygdx.entities.Player;
 import com.mygdx.map.TileMapCollisionsManager;
 import com.mygdx.stage.GCStage;
 
@@ -13,7 +13,7 @@ import com.mygdx.stage.GCStage;
  */
 public class PlayerMovement {
 
-    private final Actor player;
+    private final Player player;
     private String lastDirection;
 
     public PlayerMovement() {
@@ -25,6 +25,69 @@ public class PlayerMovement {
      * moves the player and returns the correct direction of the body
      */
     public String move() {
+        return player.isFighting() ? moveWhileFighting() : moveWhileNotFighting();
+    }
+
+    /***
+     * Called when fucking while mobbing
+     */
+    private String moveWhileFighting() {
+        float angle = CameraController.getMouseAngle();
+        String idleDir = "-";
+
+        if (angle > 55 && angle <= 125)
+            idleDir = "IDLE_UP";
+        else if (angle > 125 && angle <= 235)
+            idleDir = "IDLE_LEFT";
+        else if (angle > 235 && angle <= 305)
+            idleDir = "IDLE_DOWN";
+        else if (angle > 305 || angle <= 55)
+            idleDir = "IDLE_RIGHT";
+        String direction = "WALK_" + idleDir.substring(5);
+
+        Vector2 finalPosition = new Vector2(0, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            if (direction.equals("WALK_RIGHT"))
+                finalPosition.x += 40;
+            finalPosition.x += 100;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            if (direction.equals("WALK_LEFT"))
+                finalPosition.x -= 40;
+            finalPosition.x -= 100;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            if (direction.equals("WALK_UP"))
+                finalPosition.y += 40;
+            finalPosition.y += 100;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            if (direction.equals("WALK_DOWN"))
+                finalPosition.y -= 40;
+            finalPosition.y -= 100;
+        }
+        
+        if (TileMapCollisionsManager.getCurrentTileProprieties() == null)
+            return "";
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+            finalPosition.scl(10);
+
+        finalPosition.scl(Gdx.graphics.getDeltaTime());
+        if (TileMapCollisionsManager.canMove(player.getX() + finalPosition.x, player.getY() + finalPosition.y)) {
+            player.setX(player.getX() + finalPosition.x);
+            player.setY(player.getY() + finalPosition.y);
+        }
+
+        lastDirection = direction;
+
+        return direction;
+    }
+
+    /***
+     * Called when not fucking while mobbing
+     */
+    private String moveWhileNotFighting() {
         String direction = "IDLE_DOWN";
         Vector2 finalPosition = new Vector2(0, 0);
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
@@ -57,7 +120,7 @@ public class PlayerMovement {
         if (TileMapCollisionsManager.getCurrentTileProprieties() == null)
             return "";
 
-        if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
             finalPosition.scl(10);
 
         finalPosition.scl(Gdx.graphics.getDeltaTime());
@@ -67,6 +130,7 @@ public class PlayerMovement {
         }
 
         lastDirection = direction;
+
         return direction;
     }
 }
